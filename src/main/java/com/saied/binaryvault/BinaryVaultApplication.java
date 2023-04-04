28cf7fd7127a6ad30cabbaec74c482f01fd57232
Kirill Saied <sayed.kirill@gmail.com>
2023-04-04 17:20:57 +0300

    Initial

---
+++ src/main/java/com/saied/binaryvault/BinaryVaultApplication.java
@@ -0,0 +1,13 @@
+package com.saied.binaryvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryVaultApplication.class, args);
+    }
+
+}
