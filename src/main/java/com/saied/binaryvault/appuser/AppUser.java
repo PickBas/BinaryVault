727db6e5cd0968bc768dff4f4f8ae3b6ccaa3723
Kirill Saied <sayed.kirill@gmail.com>
2023-04-04 18:34:33 +0300

    Added AppUser table

---
+++ src/main/java/com/saied/binaryvault/appuser/AppUser.java
@@ -0,0 +1,54 @@
+package com.saied.binaryvault.appuser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+import lombok.Data;
+
+@Entity(name = "app_user")
+@Table(
+    name = "app_user",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "appuser_email_unique", columnNames = "email"),
+        @UniqueConstraint(name = "app_user_username", columnNames = "username")
+    }
+)
+@Data
+public class AppUser {
+    @Id
+    @SequenceGenerator(
+        name = "appuser_id_sequence",
+        sequenceName = "appuser_id_sequence"
+    )
+    @GeneratedValue(
+        strategy = SEQUENCE,
+        generator = "appuser_id_sequence"
+    )
+    private Long id;
+    @Column(
+        name = "username",
+        nullable = false
+    )
+    private String username;
+    @Column(
+        name = "email",
+        nullable = false
+    )
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(
+        name = "password",
+        nullable = false,
+        columnDefinition = "TEXT"
+    )
+    private String password;
+}
